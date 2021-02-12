05b18d23ed12fda59264c0c35b3e0c74c260a04d
curioswati <jaiswalswati94@gmail.com>
2021-02-12 11:05:30 +0000

    add skeleton for frontend
    - main vue instance
    - header, footer and home component

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+    <footer class="footer">
+        <p class="copyleft text-center">{{ copyleft }}</p>
+    </footer>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                copyleft: "Copyleft 2021 Bhavcopy Searchified"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    footer {
+        background: #222;
+        padding: 10px;
+        box-shadow: 2px 2px 5px 1px #f1f1f1;
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+    }
+    .copyleft {
+        color: lightgreen;
+        font-size: 1em;
+    }
+</style>
---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="home">
+        <Bhavcopy />
+        <Results />
+    </div>
+</template>
+
+<script>
+
+import Bhavcopy from '../components/Bhavcopy';
+import Results from '../components/Results';
+
+export default {
+  name: 'Home',
+  components: {
+    Bhavcopy,
+    Results,
+  },
+}
+</script>
+
+<style scoped>
+    .home {
+        min-height: 750px;
+    }
+</style>
