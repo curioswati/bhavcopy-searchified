1863782331af337e9852cd37c76f70b0c49877f3
curioswati <jaiswalswati94@gmail.com>
2021-02-12 10:34:59 +0000

    add vue and babel config

---
+++ frontend/vue.config.js
@@ -0,0 +1,15 @@
+const pages = {  
+    index: "src/main.js",  
+};  
+
+module.exports = {  
+    publicPath: "/static/vue/",
+    outputDir: "./build/static/vue/",
+    indexPath: "../../templates/vue_index.html",
+
+    pages: pages,  
+    devServer: {
+        disableHostCheck: true
+    }
+
+};
