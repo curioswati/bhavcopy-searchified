fd04bfaadce14237d47ab9f8ee4ea09016d84139
curioswati <jaiswalswati94@gmail.com>
2021-02-15 22:55:58 +0530

    add fontawesome to the project

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,7 +4,14 @@ import App from './App.vue'
 import "bootstrap"
 import "bootstrap/dist/css/bootstrap.min.css"
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faGithub)
+
 Vue.config.productionTip = false
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
     render: h => h(App),
