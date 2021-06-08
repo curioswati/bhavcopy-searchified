4b06869e09569b19d5fb4f7f5df0424df58f500b
Swati Jaiswal <jaiswalswati94@gmail.com>
2021-06-08 21:57:04 +0530

    move secret key to env

--- bhavcopy_api/bhavcopy_api/settings.py
+++ bhavcopy_api/bhavcopy_api/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e**2!mfqwka#rmys2&z+(*l7(sg!hj$ygr(2r)6asds*8^j*4y'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
